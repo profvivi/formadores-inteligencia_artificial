a06496afdf4075f1b807afd469745b4565610524
Viviane <106716101+profvivi@users.noreply.github.com>
2024-07-02 19:25:54 -0300

    salvar

--- script.js
+++ script.js
@@ -36,10 +36,10 @@ let perguntaAtual;/*variável vazia, receberá o texto da pergunta. Essa variáv
 /*Agora, para que possamos efetivamente mostrar a pergunta, vamos usar uma função. A primeira coisa que nossa função precisa fazer é colocar
 dentro da variável perguntaAtual o texto da pergunta. Então, vamos primeiro declarar a função: (aula4)*/
 function mostraPergunta() { /*declarar a função*/
-   /* if (atual >= perguntas.length) { 
+   /**/ if (atual >= perguntas.length) { 
         mostraResultado();
         return;
-    }*/
+    }
     perguntaAtual = perguntas[atual];/*instrução para receber o texto da pergunta atual.(aula4)*/
     caixaPerguntas.textContent = perguntaAtual.enunciado;/*alterarando o atributo textcontent e inserindo o atributo enunciado(aula4)*/ 
     caixaAlternativas.textContent = "";
