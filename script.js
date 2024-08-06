10326a8aa0421834f1f4f1269a424d594c0495da
Viviane <106716101+profvivi@users.noreply.github.com>
2024-08-06 19:44:27 -0300

    s

--- script.js
+++ script.js
@@ -8,23 +8,47 @@ const perguntas = [
     {
         enunciado: "O que fazer quando  identificar que uma criança está sofrendo bullying?",
         alternativas: [
-            "Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.", 
-            "Intervir e defender a vítima, conversar com o agressor."
+            {
+                texto:"Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.", 
+                afirmação:"Intervir e defender a vítima, conversar com o agressor."
+            },
+            {
+                texto:"Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.", 
+                afirmação:"Intervir e defender a vítima, conversar com o agressor."
+            }
+           // "Reportar imediatamente à equipe escolar e oferecer apoio emocional, garantindo que o aluno saiba que tem suporte e não está sozinho.", 
+           // "Intervir e defender a vítima, conversar com o agressor."
         ]
     },
     {
         enunciado: "A prática regular  de um esporte melhora  o condicionamento físico e a socialização. Você pratica algum esporte regularmente?",
         alternativas: [
-            "Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
+            {
+                texto:"Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
+                afirmação:"O esporte é uma atividade competitiva que tem regras fixas e treinamentos e pratico regularmente."
+            },
+            {
+                texto:"Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
+                afirmação:"O esporte é uma atividade competitiva que tem regras fixas e treinamentos e pratico regularmente."
+            }
+           // "Eu não pratico esportes da maneira convencional, mas amo o esporte como meio para socialização.",
  
-            "O esporte é uma atividade competitiva que tem regras fixas e treinamentos e pratico regularmente."
+            //"O esporte é uma atividade competitiva que tem regras fixas e treinamentos e pratico regularmente."
         ]
     },
     {
         enunciado: "A energia solar é uma fonte de energia renovável e sustentável que é obtida a partir da luz do sol. Diante disso eu:",
         alternativas: [
-            "Investiria em energia solar", 
-            "Iria dar continuade ao uso da energia hidrelétrica"
+            {
+                texto:"Investiria em energia solar", 
+                afirmação: "Iria dar continuade ao uso da energia hidrelétrica"
+            },
+            {
+                texto:"Investiria em energia solar", 
+                afirmação: "Iria dar continuade ao uso da energia hidrelétrica"
+            }
+           // "Investiria em energia solar", 
+           // "Iria dar continuade ao uso da energia hidrelétrica"
         ]
     }  
 ];
@@ -42,8 +66,8 @@ function mostraPergunta(){
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
+       // const botaoAlternativas = document.createElement("button");
+       // botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", function(){
             atual++;
             mostraPergunta();
