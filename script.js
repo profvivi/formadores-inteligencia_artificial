337cdc1da415e6de6539f2f5efb82fc34c53d837
Viviane <106716101+profvivi@users.noreply.github.com>
2024-08-13 20:01:28 -0300

    s

--- script.js
+++ script.js
@@ -70,9 +70,9 @@ function mostraPergunta(){
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
-       // const botaoAlternativas = document.createElement("button");
-       // botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function(){
+       const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+       botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
             atual++;
             mostraPergunta();
         });
